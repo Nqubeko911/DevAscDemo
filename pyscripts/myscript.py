1adf5afc8b376e7ba0725648b76fd5404a5563c0
Nqubeko Mathonsi <nqubeko.mathonsi25@gmail.com>
2026-02-17 07:27:18 +0200

    created basic python scripts file and folder

---
+++ pyscripts/myscript.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+A basic Python script template.
+"""
+
+
+def main():
+    """Main entry point of the script."""
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
